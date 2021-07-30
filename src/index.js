645216fb3c9d57778df34c9e4cb2630c294b48f1
Russels-teapot <baxarev.777@yandex.ru>
2021-07-30 17:30:51 +0300

    Выполнение домашней работы

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import read from './js/reader';
 import json from './js/parser';
 import GameSavingLoader from './js/app';
+import GameSaving from './js/GameSaving';
--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,17 @@
 import read from './reader';
 import json from './parser';
+import GameSaving from './GameSaving';
 
+const gameSaving = new GameSaving();
 export default class GameSavingLoader {
-  static load() {
-    (async () => {
-      try {
-        const result = await read();
-        const parsedResult = await json(result);
-        console.log(parsedResult);
-      } catch (error) {
-        console.log(error);
-      }
-    })();
+  static async load() {
+    try {
+      const result = await read();
+      const parsedResult = await json(result);
+      console.log(parsedResult);
+    } catch (error) {
+      console.log(error);
+    }
+    return gameSaving;
   }
 }
